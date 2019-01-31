83636e3dd0d74a1263a453dd750db8f0fef5ec10
José Ignacio Amelivia Santiago <ohcan2@gmail.com>
2019-01-31 09:59:43 +0100

    Decomposing the script in functions

--- movie-quote-twitter-bot/config.example.py
+++ movie-quote-twitter-bot/config.example.py
@@ -1,7 +1,7 @@
 GENERAL_CONFIG = {
     'subsURI': 'your-subtitles-file',
     'videoURI': 'your-video-file',
-    'outputPath': 'output-path-for-the-GIF',
+    'outputURI': 'output-path-for-the-GIF',
     'consumerKey': 'your-twitter-account-consumer-key',
     'consumerSecret': 'your-twitter-account-consumer-secret',
     'accessTokenKey': 'your-access-token-key',
--- movie-quote-twitter-bot/run.py
+++ movie-quote-twitter-bot/run.py
@@ -4,23 +4,35 @@ import random
 import twitter
 import config
 from moviepy.editor import *
-subsFile = open(config.GENERAL_CONFIG['subsURI'], encoding='utf-8-sig')
-subs = list(srt.parse(subsFile.read()))
-selected = random.choice(subs)
-video = (VideoFileClip(config.GENERAL_CONFIG['videoURI'])
-         .subclip(str(selected.start), str(selected.end))
-         .resize(0.3))
-text = TextClip(selected.content, fontsize=24, color='yellow',
-                font='Amiri-bold').set_pos('bottom').set_duration(str(selected.end - selected.start))
-compo = CompositeVideoClip([video, text])
-compo.write_gif(config.GENERAL_CONFIG['outputPath'])
+subs = get_subs(config.GENERAL_CONFIG['subsURI'])
+quote = random.choice(subs)
+video = generate_video_clip(config.GENERAL_CONFIG['videoURI'], quote.start, quote.end)
+text = generate_text_clip(quote.content, quote.start, quote.end)
+create_gif(config.GENERAL_CONFIG['outputURI'], video, text)
+
 api = twitter.Api(
     consumer_key=config.GENERAL_CONFIG['consumerKey'],
     consumer_secret=config.GENERAL_CONFIG['consumerSecret'],
     access_token_key=config.GENERAL_CONFIG['accessTokenKey'],
     access_token_secret=config.GENERAL_CONFIG['accessTokenSecret'],
 )
-status = api.PostUpdate(
-    selected.content,
-    media=config.GENERAL_CONFIG['outputPath'],
-    media_category='tweet_gif')
+post_gif_to_twitter(api, quote.content, config.GENERAL_CONFIG['outputURI'])
+
+def get_subs(subs_file_uri):
+    subs_file = open(config.GENERAL_CONFIG['subsURI'], encoding='utf-8-sig')
+    return list(srt.parse(subs_file.read()))
+
+def post_gif_to_twitter(api, sentence, gif_path):
+    api.PostUpdate(sentence, media=gif_path, media_category='tweet_gif')
+
+def generate_video_clip(video_uri, start, end):
+    return (VideoFileClip(video_uri)
+            .subclip(str(start), str(end))
+            .resize(0.3))
+
+def generate_text_clip(sentence, start, end):
+    return TextClip(sentence, fontsize=24, color='yellow',font='Amiri-bold').set_pos('bottom').set_duration(str(end - start))
+
+def create_gif(output_uri, video, text):
+    compo = CompositeVideoClip([video, text])
+    compo.write_gif(output_uri)
