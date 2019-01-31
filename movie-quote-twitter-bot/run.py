f6fb102e4806b7f5d34782774f5594846beffe6b
José Ignacio Amelivia Santiago <ohcan2@gmail.com>
2019-01-31 10:00:17 +0100

    Appying indentation fixes

--- movie-quote-twitter-bot/run.py
+++ movie-quote-twitter-bot/run.py
@@ -6,7 +6,10 @@ import config
 from moviepy.editor import *
 subs = get_subs(config.GENERAL_CONFIG['subsURI'])
 quote = random.choice(subs)
-video = generate_video_clip(config.GENERAL_CONFIG['videoURI'], quote.start, quote.end)
+video = generate_video_clip(
+    config.GENERAL_CONFIG['videoURI'],
+    quote.start,
+    quote.end)
 text = generate_text_clip(quote.content, quote.start, quote.end)
 create_gif(config.GENERAL_CONFIG['outputURI'], video, text)
 
@@ -18,20 +21,26 @@ api = twitter.Api(
 )
 post_gif_to_twitter(api, quote.content, config.GENERAL_CONFIG['outputURI'])
 
+
 def get_subs(subs_file_uri):
     subs_file = open(config.GENERAL_CONFIG['subsURI'], encoding='utf-8-sig')
     return list(srt.parse(subs_file.read()))
 
+
 def post_gif_to_twitter(api, sentence, gif_path):
     api.PostUpdate(sentence, media=gif_path, media_category='tweet_gif')
+
 
 def generate_video_clip(video_uri, start, end):
     return (VideoFileClip(video_uri)
             .subclip(str(start), str(end))
             .resize(0.3))
 
+
 def generate_text_clip(sentence, start, end):
-    return TextClip(sentence, fontsize=24, color='yellow',font='Amiri-bold').set_pos('bottom').set_duration(str(end - start))
+    return TextClip(sentence, fontsize=24, color='yellow',
+                    font='Amiri-bold').set_pos('bottom').set_duration(str(end - start))
+
 
 def create_gif(output_uri, video, text):
     compo = CompositeVideoClip([video, text])
