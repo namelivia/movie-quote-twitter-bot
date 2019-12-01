a039df12850ece055dd4453c8809e2c97b8a81be
José Ignacio Amelivia <ohcan2@gmail.com>
2019-12-01 14:52:07 +0100

    Using black for code formatting

--- movie_quote_twitter_bot/movie_quote_twitter_bot.py
+++ movie_quote_twitter_bot/movie_quote_twitter_bot.py
@@ -7,48 +7,51 @@ from moviepy.editor import *
 
 
 class MovieQuoteTwitterBot:
-
     def main(self):
-        quote = random.choice(self.get_subs(config.GENERAL_CONFIG['subsURI']))
+        quote = random.choice(self.get_subs(config.GENERAL_CONFIG["subsURI"]))
         video = self.generate_video_clip(
-            config.GENERAL_CONFIG['videoURI'],
-            quote.start,
-            quote.end
+            config.GENERAL_CONFIG["videoURI"], quote.start, quote.end
         )
         text = self.generate_text_clip(quote.content, quote.start, quote.end)
-        self.create_gif(config.GENERAL_CONFIG['outputURI'], video, text)
+        self.create_gif(config.GENERAL_CONFIG["outputURI"], video, text)
 
         api = twitter.Api(
-            consumer_key=config.GENERAL_CONFIG['consumerKey'],
-            consumer_secret=config.GENERAL_CONFIG['consumerSecret'],
-            access_token_key=config.GENERAL_CONFIG['accessTokenKey'],
-            access_token_secret=config.GENERAL_CONFIG['accessTokenSecret'],
+            consumer_key=config.GENERAL_CONFIG["consumerKey"],
+            consumer_secret=config.GENERAL_CONFIG["consumerSecret"],
+            access_token_key=config.GENERAL_CONFIG["accessTokenKey"],
+            access_token_secret=config.GENERAL_CONFIG["accessTokenSecret"],
         )
-        self.post_gif_to_twitter(
-            api, quote.content, config.GENERAL_CONFIG['outputURI'])
+        self.post_gif_to_twitter(api, quote.content, config.GENERAL_CONFIG["outputURI"])
 
     def get_subs(self, subs_file_uri):
         subs_file = open(
-            config.GENERAL_CONFIG['subsURI'],
-            encoding=config.GENERAL_CONFIG['subsEncoding'])
+            config.GENERAL_CONFIG["subsURI"],
+            encoding=config.GENERAL_CONFIG["subsEncoding"],
+        )
         return list(srt.parse(subs_file.read()))
 
     def post_gif_to_twitter(self, api, sentence, gif_path):
-        api.PostUpdate(sentence, media=gif_path, media_category='tweet_gif')
+        api.PostUpdate(sentence, media=gif_path, media_category="tweet_gif")
 
     def generate_video_clip(self, video_uri, start, end):
-        return (VideoFileClip(video_uri)
-                .subclip(str(start), str(end))
-                .resize(0.3))
+        return VideoFileClip(video_uri).subclip(str(start), str(end)).resize(0.3)
 
     def generate_text_clip(self, sentence, start, end):
-        return TextClip(sentence, fontsize=config.GENERAL_CONFIG['textSize'], color=config.GENERAL_CONFIG['textColor'],
-                        font=config.GENERAL_CONFIG['textFont']).set_pos('bottom').set_duration(str(end - start))
+        return (
+            TextClip(
+                sentence,
+                fontsize=config.GENERAL_CONFIG["textSize"],
+                color=config.GENERAL_CONFIG["textColor"],
+                font=config.GENERAL_CONFIG["textFont"],
+            )
+            .set_pos("bottom")
+            .set_duration(str(end - start))
+        )
 
     def create_gif(self, output_uri, video, text):
         compo = CompositeVideoClip([video, text])
         compo.write_gif(output_uri, verbose=False, progress_bar=False)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     MovieQuoteTwitterBot().main()
--- setup.py
+++ setup.py
@@ -1,18 +1,14 @@
 from setuptools import setup
 
 setup(
-    name='MovieQuoteTwitterBot',
-    author='José Ignacio Amelivia Santiago',
-    author_email='jignacio.amelivia@gmail.com',
-    url='https://namelivia.com',
-    description='This is a small python bot for posting movie quotes',
-    license='LICENSE',
-    long_description=open('README.md').read(),
-    packages=['movie_quote_twitter_bot'],
+    name="MovieQuoteTwitterBot",
+    author="José Ignacio Amelivia Santiago",
+    author_email="jignacio.amelivia@gmail.com",
+    url="https://namelivia.com",
+    description="This is a small python bot for posting movie quotes",
+    license="LICENSE",
+    long_description=open("README.md").read(),
+    packages=["movie_quote_twitter_bot"],
     include_package_data=True,
-    install_requires=[
-        'moviepy >= 0.2',
-        'srt >= 1.9',
-        'python-twitter >= 3.5',
-    ],
+    install_requires=["moviepy >= 0.2", "srt >= 1.9", "python-twitter >= 3.5",],
 )
