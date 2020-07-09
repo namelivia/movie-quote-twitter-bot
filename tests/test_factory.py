9522779a5c687858f77c711745bbf71ebfc5f52b
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-07-09 23:13:19 +0200

    Add factory test

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,11 +1,41 @@
 from unittest import TestCase
 from movie_quote_twitter_bot.factory import Factory
+from movie_quote_twitter_bot.bot import Bot
+import mock
 
 
 class TestTwitter(TestCase):
-    def setUp(self):
-        self.config = {}
-        self.factory = Factory(self.config)
 
-    def test_building_bot(self):
-        self.factory.build()
+    @mock.patch('movie_quote_twitter_bot.factory.Subs')
+    @mock.patch('movie_quote_twitter_bot.factory.TextClip')
+    @mock.patch('movie_quote_twitter_bot.factory.VideoClip')
+    @mock.patch('movie_quote_twitter_bot.factory.Twitter')
+    @mock.patch('movie_quote_twitter_bot.factory.Gif')
+    def test_building_bot(
+        self,
+        m_gif,
+        m_twitter,
+        m_video_clip,
+        m_text_clip,
+        m_subs
+    ):
+        config = mock.Mock()
+        config.get.return_value = 'config_value'
+        factory = Factory(config)
+        bot = factory.build()
+        m_subs.assert_called_once_with('config_value', 'config_value')
+        m_text_clip.assert_called_once_with(
+            'config_value',
+            'config_value',
+            'config_value'
+        )
+        m_video_clip.assert_called_once_with('config_value')
+        m_twitter.assert_called_once_with(
+            'config_value',
+            'config_value',
+            'config_value',
+            'config_value',
+            'config_value'
+        )
+        m_gif.assert_called_once_with('config_value')
+        self.assertIsInstance(bot, Bot)
